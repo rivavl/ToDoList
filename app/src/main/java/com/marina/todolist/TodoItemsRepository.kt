a2564a994523f15c35dca2a13dae487d97cd3672
rivavl <mvkalinenkova@yandex.ru>
2022-07-31 22:04:01 +0300

    Add repository

---
+++ app/src/main/java/com/marina/todolist/TodoItemsRepository.kt
@@ -0,0 +1,133 @@
+package com.marina.todolist
+
+class TodoItemsRepository {
+
+    val list = mutableListOf(
+        TodoItem(
+            id = "1",
+            text = "",
+            importance = TodoItem.Importance.LOW,
+            deadline = 1662069354000,
+            isDone = false,
+            creationDate = 1659304554000,
+            lastChangeDate = 1659304554003
+        ),
+        TodoItem(
+            id = "1w",
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            importance = TodoItem.Importance.HIGH,
+            deadline = 1283378154000,
+            isDone = true,
+            creationDate = 809992554000,
+            lastChangeDate = 1659304554003
+        ),
+        TodoItem(
+            id = "1wq",
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            importance = TodoItem.Importance.ORDINARY,
+            deadline = 1693605354000,
+            isDone = true,
+            creationDate = 1656712554000,
+            lastChangeDate = null
+        ),
+        TodoItem(
+            id = "1w2q",
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            importance = TodoItem.Importance.LOW,
+            deadline = 1693605354000,
+            isDone = true,
+            creationDate = 1656712554000,
+            lastChangeDate = null
+        ),
+        TodoItem(
+            id = "1w2wq",
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            importance = TodoItem.Importance.LOW,
+            deadline = 1693605354000,
+            isDone = true,
+            creationDate = 1656712554000,
+            lastChangeDate = null
+        ),
+        TodoItem(
+            id = "1w21q",
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            importance = TodoItem.Importance.LOW,
+            deadline = 1693605354000,
+            isDone = true,
+            creationDate = 1656712554000,
+            lastChangeDate = null
+        ),
+        TodoItem(
+            id = "1w24q",
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            importance = TodoItem.Importance.LOW,
+            deadline = 1693605354000,
+            isDone = true,
+            creationDate = 1656712554000,
+            lastChangeDate = null
+        ),
+        TodoItem(
+            id = "1w222q",
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            importance = TodoItem.Importance.LOW,
+            deadline = 1693605354000,
+            isDone = true,
+            creationDate = 1656712554000,
+            lastChangeDate = null
+        ),
+        TodoItem(
+            id = "1w21112q",
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            importance = TodoItem.Importance.LOW,
+            deadline = 1693605354000,
+            isDone = true,
+            creationDate = 1656712554000,
+            lastChangeDate = null
+        ),
+        TodoItem(
+            id = "1wde2q",
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            importance = TodoItem.Importance.LOW,
+            deadline = 1693605354000,
+            isDone = true,
+            creationDate = 1656712554000,
+            lastChangeDate = null
+        ),
+        TodoItem(
+            id = "1wdwdw2q",
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            importance = TodoItem.Importance.LOW,
+            deadline = 1693605354000,
+            isDone = true,
+            creationDate = 1656712554000,
+            lastChangeDate = null
+        ),
+        TodoItem(
+            id = "khg",
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            importance = TodoItem.Importance.LOW,
+            deadline = 1693605354000,
+            isDone = true,
+            creationDate = 1656712554000,
+            lastChangeDate = null
+        ),
+        TodoItem(
+            id = "3434334",
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            importance = TodoItem.Importance.LOW,
+            deadline = 1693605354000,
+            isDone = true,
+            creationDate = 1656712554000,
+            lastChangeDate = null
+        )
+    )
+
+    fun getTodoItemsList(): List<TodoItem> {
+        return list.toList()
+    }
+
+    fun addItem(todoItem: TodoItem) {
+        list.add(todoItem)
+    }
+
+}
